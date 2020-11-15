ecfaaf6debde0c0450ebf0ce866c21d0c5ede55e
Aurelien Gremy <agremy@solutionsmedias360.com>
2020-11-15 18:27:48 -0500

    refactor(begining) create files and minimum config

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # rpg-VIF
+
 RPG classique avec les VIF
+
+Création des fichiers nécessaire | Init npm and Git
+
+- Idée simple et facile :
+- - Monter un questionnaire pour le choix de chacun (pouvoir et autres spécificité)
+- - Monter liste d'info de chaque personnage
+- - Penser à la sélection du personnage
---
+++ js/script.js
@@ -0,0 +1,18 @@
+class Personnage {
+    constructor(nom, classes, signe, sante, attaque) {
+        this.nom = nom;
+        this.classes = classes;
+        this.signe = signe;
+        this.sante = sante;
+        this.attque = attaque;
+    }
+}
+
+
+// --- Element - Variable --- // 
+
+// -> Sante
+var upperLife = document.querySelector('.progress-upper');
+let downLife =  document.querySelector('.progress-down');
+
+
